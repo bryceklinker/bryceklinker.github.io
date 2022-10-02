4ef77453da85a0f97c7e10204f35b68d24655c70
Bryce Klinker <bryce.klinker@gmail.com>
2022-10-01 21:40:14 -0500

    starting initial repo

---
+++ gatsby-config.ts
@@ -0,0 +1,12 @@
+import type { GatsbyConfig } from 'gatsby';
+
+const config: GatsbyConfig = {
+    siteMetadata: {
+        title: 'Bryce Klinker'
+    },
+    plugins: [
+        'gatsby-plugin-preact',
+    ]
+};
+
+export default config;
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    jsxSingleQuote: true,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'none'
+};
